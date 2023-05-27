5ae7867bbb055e83d2087394fd3d887a45c21cc9
Ryan Adriano <ryan.adriano@outlook.com>
2023-05-27 01:12:23 -0500

    Update prepare script and add live streaming component file.

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import App from './App'
-import { createRoot } from 'react-dom/client';
+import { createRoot } from 'react-dom/client'
 
 const dom = document.getElementById('root')!
 const root = createRoot(dom)
