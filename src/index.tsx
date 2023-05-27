0224d55f388ffacd8f49f152c1ddd9bb743495f5
Ryan Adriano <ryan.adriano@outlook.com>
2023-05-27 01:05:36 -0500

    Use createRoot.

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,7 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
 import App from './App'
+import { createRoot } from 'react-dom/client';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App></App>
-  </React.StrictMode>,
-  document.getElementById('root')
-)
+const dom = document.getElementById('root')!
+const root = createRoot(dom)
+root.render(<App></App>)
